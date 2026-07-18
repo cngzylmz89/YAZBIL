5a434cfe43b16f636431411a8cca4eb719fe702a
cngzylmz89 <muallimiturki@gmail.com>
2026-07-18 21:32:47 +0300

    firstcommit

---
+++ GUZELYAZIDERSI/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace GUZELYAZIDERSI
+{
+    partial class frmYaziDegerlendir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmYaziDegerlendir));
+            this.splana = new System.Windows.Forms.SplitContainer();
+            this.splanaust = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbtnyeni = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnac = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnkaydet = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnfarklikaydet = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnyazdir = new System.Windows.Forms.ToolStripButton();
+            this.tsbtngeri = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnileri = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnrapor = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnayarlar = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splana)).BeginInit();
+            this.splana.Panel1.SuspendLayout();
+            this.splana.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splanaust)).BeginInit();
+            this.splanaust.Panel1.SuspendLayout();
+            this.splanaust.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splana
+            // 
+            this.splana.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splana.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splana.Location = new System.Drawing.Point(0, 0);
+            this.splana.Name = "splana";
+            this.splana.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splana.Panel1
+            // 
+            this.splana.Panel1.Controls.Add(this.splanaust);
+            this.splana.Size = new System.Drawing.Size(1279, 879);
+            this.splana.SplitterDistance = 585;
+            this.splana.TabIndex = 1;
+            // 
+            // splanaust
+            // 
+            this.splanaust.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splanaust.Location = new System.Drawing.Point(0, 0);
+            this.splanaust.Name = "splanaust";
+            // 
+            // splanaust.Panel1
+            // 
+            this.splanaust.Panel1.Controls.Add(this.tableLayoutPanel1);
+            this.splanaust.Size = new System.Drawing.Size(1279, 585);
+            this.splanaust.SplitterDistance = 1023;
+            this.splanaust.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.toolStrip1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1023, 585);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnyeni,
+            this.tsbtnac,
+            this.tsbtnkaydet,
+            this.tsbtnfarklikaydet,
+            this.tsbtnyazdir,
+            this.tsbtngeri,
+            this.tsbtnileri,
+            this.tsbtnrapor,
+            this.tsbtnayarlar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1023, 58);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbtnyeni
+            // 
+            this.tsbtnyeni.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnyeni.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tsbtnyeni.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnyeni.Image")));
+            this.tsbtnyeni.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnyeni.Name = "tsbtnyeni";
+            this.tsbtnyeni.Padding = new System.Windows.Forms.Padding(10);
+            this.tsbtnyeni.Size = new System.Drawing.Size(44, 55);
+            this.tsbtnyeni.Text = "YENİ";
+            this.tsbtnyeni.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.tsbtnyeni.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            this.tsbtnyeni.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // tsbtnac
+            // 
+            this.tsbtnac.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnac.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnac.Image")));
+            this.tsbtnac.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnac.Name = "tsbtnac";
+            this.tsbtnac.Size = new System.Drawing.Size(29, 55);
+            this.tsbtnac.Text = "AÇ";
+            // 
+            // tsbtnkaydet
+            // 
+            this.tsbtnkaydet.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnkaydet.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnkaydet.Image")));
+            this.tsbtnkaydet.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnkaydet.Name = "tsbtnkaydet";
+            this.tsbtnkaydet.Size = new System.Drawing.Size(29, 55);
+            this.tsbtnkaydet.Text = "KAYDET";
+            // 
+            // tsbtnfarklikaydet
+            // 
+            this.tsbtnfarklikaydet.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnfarklikaydet.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnfarklikaydet.Image")));
+            this.tsbtnfarklikaydet.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnfarklikaydet.Name = "tsbtnfarklikaydet";
+            this.tsbtnfarklikaydet.Size = new System.Drawing.Size(29, 55);
+            this.tsbtnfarklikaydet.Text = "FARKLI KAYDET";
+            // 
+            // tsbtnyazdir
+            // 
+            this.tsbtnyazdir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnyazdir.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnyazdir.Image")));
+            this.tsbtnyazdir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnyazdir.Name = "tsbtnyazdir";
+            this.tsbtnyazdir.Size = new System.Drawing.Size(29, 55);
+            this.tsbtnyazdir.Text = "YAZDIR";
+            // 
+            // tsbtngeri
+            // 
+            this.tsbtngeri.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtngeri.Image = ((System.Drawing.Image)(resources.GetObject("tsbtngeri.Image")));
+            this.tsbtngeri.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtngeri.Name = "tsbtngeri";
+            this.tsbtngeri.Size = new System.Drawing.Size(29, 55);
+            this.tsbtngeri.Text = "ÖNCEKİ";
+            // 
+            // tsbtnileri
+            // 
+            this.tsbtnileri.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnileri.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnileri.Image")));
+            this.tsbtnileri.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnileri.Name = "tsbtnileri";
+            this.tsbtnileri.Size = new System.Drawing.Size(29, 55);
+            this.tsbtnileri.Text = "SONRAKİ";
+            // 
+            // tsbtnrapor
+            // 
+            this.tsbtnrapor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnrapor.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnrapor.Image")));
+            this.tsbtnrapor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnrapor.Name = "tsbtnrapor";
+            this.tsbtnrapor.Size = new System.Drawing.Size(29, 55);
+            this.tsbtnrapor.Text = "RAPOR";
+            this.tsbtnrapor.ToolTipText = "RAPOR";
+            // 
+            // tsbtnayarlar
+            // 
+            this.tsbtnayarlar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnayarlar.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnayarlar.Image")));
+            this.tsbtnayarlar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnayarlar.Name = "tsbtnayarlar";
+            this.tsbtnayarlar.Size = new System.Drawing.Size(29, 55);
+            this.tsbtnayarlar.Text = "AYARLAR";
+            // 
+            // frmYaziDegerlendir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1279, 879);
+            this.Controls.Add(this.splana);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmYaziDegerlendir";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YAZBİL - Yazı Değerlendirme";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmYaziDegerlendir_Load);
+            this.splana.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splana)).EndInit();
+            this.splana.ResumeLayout(false);
+            this.splanaust.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splanaust)).EndInit();
+            this.splanaust.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splana;
+        private System.Windows.Forms.SplitContainer splanaust;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbtnyeni;
+        private System.Windows.Forms.ToolStripButton tsbtnac;
+        private System.Windows.Forms.ToolStripButton tsbtnkaydet;
+        private System.Windows.Forms.ToolStripButton tsbtnfarklikaydet;
+        private System.Windows.Forms.ToolStripButton tsbtnyazdir;
+        private System.Windows.Forms.ToolStripButton tsbtngeri;
+        private System.Windows.Forms.ToolStripButton tsbtnileri;
+        private System.Windows.Forms.ToolStripButton tsbtnrapor;
+        private System.Windows.Forms.ToolStripButton tsbtnayarlar;
+    }
+}
+
---
+++ GUZELYAZIDERSI/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUZELYAZIDERSI
+{
+    public partial class frmYaziDegerlendir : Form
+    {
+        public frmYaziDegerlendir()
+        {
+            InitializeComponent();
+        }
+
+        public static class ButtonSinifi
+        {
+            public static void ToolStripButtonAyarla(ToolStripButton btn)
+            {
+               
+                btn.Enabled = true;
+                btn.Visible = true;
+                
+                btn.DisplayStyle = ToolStripItemDisplayStyle.ImageAndText;
+                btn.ImageAlign = ContentAlignment.TopCenter;
+                btn.TextImageRelation = TextImageRelation.ImageAboveText;
+
+                btn.DisplayStyle = ToolStripItemDisplayStyle.ImageAndText;
+                btn.TextImageRelation = TextImageRelation.ImageAboveText;
+
+               btn.Padding = new Padding(9);
+
+            }
+        }
+
+      
+       
+        private void frmYaziDegerlendir_Load(object sender, EventArgs e)
+        {
+
+            ButtonSinifi.ToolStripButtonAyarla(tsbtnyeni);
+            ButtonSinifi.ToolStripButtonAyarla(tsbtnac);
+            ButtonSinifi.ToolStripButtonAyarla(tsbtnkaydet);
+            ButtonSinifi.ToolStripButtonAyarla(tsbtnfarklikaydet);
+            ButtonSinifi.ToolStripButtonAyarla(tsbtnyazdir);
+            ButtonSinifi.ToolStripButtonAyarla(tsbtngeri);
+            ButtonSinifi.ToolStripButtonAyarla(tsbtnileri);
+            ButtonSinifi.ToolStripButtonAyarla(tsbtnrapor);
+            ButtonSinifi.ToolStripButtonAyarla(tsbtnayarlar);
+
+        }
+    }
+}
